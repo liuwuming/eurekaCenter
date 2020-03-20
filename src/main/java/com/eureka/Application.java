3724c42d257d5ce7b6809cf7b84ba90bb7f0ad7b
liuwuming <534881840@qq.com>
2020-03-20 19:21:44 +0800

    更新eureka

--- src/main/java/com/eureka/Application.java
+++ src/main/java/com/eureka/Application.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServiceCenterApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceCenterApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
